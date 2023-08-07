de98f3197ee2729787a7002a0a4e7ba131fea47a
helen yunita <94655932+Helenyunita@users.noreply.github.com>
2023-08-07 11:17:13 +0700

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,49 @@
+def penjumlahan(a, b):
+    return a + b
+
+def pengurangan(a, b):
+    return a - b
+
+def perkalian(a, b):
+    return a * b
+
+def pembagian(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "pembagian tidak bisa menggunakan 0"
+
+print("---Kalkulator Sederhana---")
+print("--------------------------")
+
+while True:
+    print("Pilih operasi:")
+    print("1. Penjumlahan")
+    print("2. Pengurangan")
+    print("3. Perkalian")
+    print("4. Pembagian")
+    print("5. Keluar")
+
+    pilihan = input("Masukkan pilihan 1/2/3/4/5: ")
+
+    if pilihan == '5':
+        print("anda keluar dari kalkulator")
+        break
+
+    angka1 = float(input("Masukkan angka pertama: "))
+    angka2 = float(input("Masukkan angka kedua: "))
+
+    if pilihan == '1':
+        hasil = penjumlahan(angka1, angka2)
+        print("Hasil penjumlahan:", hasil)
+    elif pilihan == '2':
+        hasil = pengurangan(angka1, angka2)
+        print("Hasil pengurangan:", hasil)
+    elif pilihan == '3':
+        hasil = perkalian(angka1, angka2)
+        print("Hasil perkalian:", hasil)
+    elif pilihan == '4':
+        hasil = pembagian(angka1, angka2)
+        print("Hasil pembagian:", hasil)
+    else:
+        print("Pilihan tidak valid. Silakan masukkan pilihan yang sesuai.")
